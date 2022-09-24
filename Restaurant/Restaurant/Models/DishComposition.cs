34807e78749d920b84f0241154240d70690e12e7
Valeriaaankaa <v.rudoy02@gmail.com>
2022-09-24 23:34:25 +0300

    Created class DishComposition

---
+++ Restaurant/Restaurant/Models/DishComposition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Models
+{
+    public class DishComposition
+    {
+        public int Id { get; set; }
+        public Dish Dish { get; set; }
+        public Ingredient Ingredient { get; set; }
+        public decimal amount { get; set; }
+    }
+}
