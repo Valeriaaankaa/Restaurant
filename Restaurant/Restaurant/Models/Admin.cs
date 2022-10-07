03e53a114ba977cc5d0d76eb3be8378986721d2e
Valeriaaankaa <v.rudoy02@gmail.com>
2022-10-08 00:13:04 +0300

    Create class Admin

---
+++ Restaurant/Restaurant/Models/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Restaurant
+{
+    /// <summary>
+    /// Information about Admin of the system
+    /// </summary>
+
+    public class Admin 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
+	
