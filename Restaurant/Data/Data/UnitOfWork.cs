fe25a18379a8a08be4d87895780193c1f596683a
S0mmerfeld <olegyevseienko@gmail.com>
2022-10-29 20:08:22 +0300

    Add classes. Add Layers

---
+++ Restaurant/Data/Data/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using Data.Interfaces;
+using Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private RestaurantDbContext _context;
+        private IngredientRepository _ingredients;
+
+        public UnitOfWork(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        public ICustomerRepository CustomerRepository
+        {
+            get
+            {
+                return _ingredients ??= new IngredientRepository(_context);
+            }
+        }
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
---
+++ Restaurant/Data/Interfaces/IIngredientRepository.cs
@@ -0,0 +1,11 @@
+﻿using Data.Entities;
+
+namespace Data.Interfaces
+{
+    public interface IIngredientRepository : IRepository<Ingredient>
+    {
+        Task<IEnumerable<Ingredient>> GetAllWithDetailsAsync();
+
+        Task<Ingredient> GetByIdWithDetailsAsync(int id);
+    }
+}
