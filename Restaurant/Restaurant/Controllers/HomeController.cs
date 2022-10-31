2a4df7b28b9b5960046647b1b59352597b1d21da
S0mmerfeld <olegyevseienko@gmail.com>
2022-10-31 22:04:17 +0200

    Add page menu to HomeController  View. Change navbar from default to ours in _Layout

--- Restaurant/Restaurant/Controllers/HomeController.cs
+++ Restaurant/Restaurant/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace Restaurant.Controllers
             return View();
         }
 
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
