bd6116e472ea7e23c80f96069c07e6b53a7ea30b
Vladyslav Zhukov <epicmand.mi@gmail.com>
2022-11-06 21:21:02 +0200

    RES-29 Added shopping cart view and model

--- Restaurant/Restaurant/Program.cs
+++ Restaurant/Restaurant/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddControllersWithViews();
 var connectionString = builder.Configuration.GetConnectionString("Restaurant");
 builder.Services.AddDbContext<RestaurantDbContext>(x => x.UseSqlServer(connectionString));
 
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddTransient<IIngredientService, IngredientService>();
 builder.Services.AddTransient<IDishService, DishService>();
