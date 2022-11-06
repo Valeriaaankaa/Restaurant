caa4f434f25952d0c363bcc6a63039f9df55c064
Valeriaaankaa <v.rudoy02@gmail.com>
2022-11-06 16:38:45 +0200

    RES-28 Added Migrations to ingredients and IngredientGroup

---
+++ Restaurant/Data/Migrations/20221106143347_ingredgroup.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class ingredgroup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredients_Dishes_DishId",
+                table: "Ingredients");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DishId",
+                table: "Ingredients",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "IngredientGroup",
+                table: "Ingredients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DishId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DishId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DishId",
+                value: null);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredients_Dishes_DishId",
+                table: "Ingredients",
+                column: "DishId",
+                principalTable: "Dishes",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredients_Dishes_DishId",
+                table: "Ingredients");
+
+            migrationBuilder.DropColumn(
+                name: "IngredientGroup",
+                table: "Ingredients");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DishId",
+                table: "Ingredients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DishId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DishId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DishId",
+                value: 3);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredients_Dishes_DishId",
+                table: "Ingredients",
+                column: "DishId",
+                principalTable: "Dishes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
