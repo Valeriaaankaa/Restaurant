f26304db6ded5c1582e87addc452ad7693d8b0bf
S0mmerfeld <olegyevseienko@gmail.com>
2022-11-08 23:32:09 +0200

    Fix bugs with DB creation

--- Restaurant/Data/Entities/Ingredient.cs
+++ Restaurant/Data/Entities/Ingredient.cs
@@ -6,5 +6,8 @@
         public decimal Amount { get; set; }
         public DateTime ImportDate { get; set; }
         public DateTime ExpirationDate { get; set; }
+
+        public decimal Price { get; set; }
+        public IngredientGroup IngredientGroup { get; set; }
     }
 }
