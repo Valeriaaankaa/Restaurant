e524b72f2ad555265f77dfedb5d4487f1d2164de
S0mmerfeld <olegyevseienko@gmail.com>
2022-11-13 20:54:44 +0200

    Customize error page

--- Restaurant/Restaurant/Program.cs
+++ Restaurant/Restaurant/Program.cs
@@ -48,6 +48,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
+
 app.UseRouting();
 
 app.UseAuthorization();
