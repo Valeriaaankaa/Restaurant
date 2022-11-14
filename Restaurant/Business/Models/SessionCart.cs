111d10c6ded67e75bb8bd65e957be9cb58205845
Vladyslav Zhukov <epicmand.mi@gmail.com>
2022-11-14 23:39:30 +0200

    Added SessionCart & separated serialization to SessionExtensions

---
+++ Restaurant/Business/Models/SessionCart.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Business.Infrastructure;
+
+
+namespace Business.Models
+{
+    public class SessionCart : Cart
+    {
+
+        public static Cart GetCart(IServiceProvider services)
+        {
+            ISession? session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+            SessionCart cart = session.GetJson<SessionCart>("cart") ?? new SessionCart();
+            cart.Session = session;
+            return cart;
+        }
+        [JsonIgnore]
+        public ISession? Session { get; set; }
+        public override void AddItem(DishModel dish, int quantity)
+        {
+            base.AddItem(dish, quantity);
+            Session.SetJson("cart", this);
+        }
+        public override void RemoveLine(DishModel dishModel)
+        {
+            base.RemoveLine(dishModel);
+            Session.SetJson("cart", this);
+        }
+        public override void Clear()
+        {
+            base.Clear();
+            Session.Remove("Cart");
+        }
+    }
+}
